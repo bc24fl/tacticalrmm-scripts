b789b0fe177422b03807380ad41ca2e7ae0a0a48
bc24fl <bc24fl@gmail.com>
2021-12-11 15:13:44 -0500

    Added script Win_Printer_Restart_Jobs.ps1

---
+++ Win_Printer_Restart_Jobs.ps1
@@ -0,0 +1,28 @@
+<#
+    .SYNOPSIS
+    Restarts stuck printer jobs.
+
+    .DESCRIPTION 
+    Cycles through each printer and restarts any jobs that are stuck with error status.
+
+    .NOTES
+    Change Log
+    ----------------------------------------------------------------------------------
+	V1.0 Initial Release by https://github.com/bc24fl/tacticalrmm-scripts/
+
+#>
+
+$AllPrinters = Get-Printer
+foreach ($Printer in $AllPrinters) {
+    $PrintJobs = Get-PrintJob -PrinterName $($Printer.Name)
+    if ($PrintJobs) {
+        foreach ($Job in $PrintJobs) {
+            if ($Job.JobStatus -match 'Error') {
+				$stuckPrinterName = $Job.PrinterName
+				$stuckPrinterJob = $Job.Id
+				Write-Host "Restarting Job Id $stuckPrinterJob on printer $stuckPrinterName"
+                Restart-PrintJob -InputObject $Job
+            }
+        }
+    }
+}
