43802820b3a22fc82362b3ed4c50860197bd5b05
bc24fl <bc24fl@gmail.com>
2022-02-17 23:56:50 -0500

    refactor

--- Win_KB_Approve_Install.ps1
+++ Win_KB_Approve_Install.ps1
@@ -15,7 +15,7 @@
     - $Kb               - Microsoft Kb number to install
    
 .EXAMPLE
-    - KB_Approve_Install.ps1 -ApiKeyTactical 1234567 -ApiUrlTactical api.yourdomain.com  -Kb "KB123456789"
+    - Win_KB_Approve_Install.ps1 -ApiKeyTactical 1234567 -ApiUrlTactical api.yourdomain.com  -Kb "KB123456789"
 		
 .VERSION
 	- v1.0 Initial Release by https://github.com/bc24fl/tacticalrmm-scripts/
